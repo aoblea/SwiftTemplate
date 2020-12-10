2fa7f6350347c275a3d893d8310dc393e01378ef
aoblea <arwinoblea@gmail.com>
2020-12-10 18:31:35 -0500

    Initial Commit

---
+++ SwiftTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTemplate
+//
+//  Created by Arwin Oblea on 12/10/20.
+//  Copyright © 2020 iarwinscott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
